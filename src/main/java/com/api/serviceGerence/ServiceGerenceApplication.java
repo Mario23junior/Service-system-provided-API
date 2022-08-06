2676145a376c44ef7cfacbdb25144ee9d7131f3b
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-06 07:22:08 -0300

    Iniciando API de gerenciamento de servicos

---
+++ src/main/java/com/api/serviceGerence/ServiceGerenceApplication.java
@@ -0,0 +1,13 @@
+package com.api.serviceGerence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGerenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceGerenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/serviceGerence/ServiceGerenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.serviceGerence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGerenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
