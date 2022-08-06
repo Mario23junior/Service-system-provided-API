6f18daf4d68c56fa5ad407783c6da114d9564481
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-06 17:33:09 -0300

    Criando entidade servico

---
+++ src/main/java/com/api/serviceGerence/model/Service.java
@@ -0,0 +1,49 @@
+package com.api.serviceGerence.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Service {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false)
+	private String descricao;
+
+	private Cliente cliente;
+
+	public Service() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+}
